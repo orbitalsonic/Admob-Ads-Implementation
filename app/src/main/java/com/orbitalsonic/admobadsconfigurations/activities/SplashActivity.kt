07cbd08353da2fcc721dea16332f152beccbdcaf
Yaqoob <yaqoob@gmail.com>
2022-12-20 14:49:20 +0500

    Ads Config

--- app/src/main/java/com/orbitalsonic/admobadsconfigurations/activities/SplashActivity.kt
+++ app/src/main/java/com/orbitalsonic/admobadsconfigurations/activities/SplashActivity.kt
@@ -162,17 +162,18 @@ class SplashActivity : AppCompatActivity() {
                 if (isInterstitialLoadOrFailed && isNativeLoadedOrFailed) {
                     binding.loadingProgress.visibility = View.GONE
                     binding.btnNext.visibility = View.VISIBLE
+                    mHandler.removeCallbacks { adsRunner }
+                }else{
+                    mHandler.removeCallbacks { adsRunner }
+                    mHandler.postDelayed(
+                        adsRunner,
+                        (1000)
+                    )
                 }
 
             } catch (e: Exception) {
                 Log.e(AD_TAG,"${e.message}")
             }
-
-            mHandler.removeCallbacks { adsRunner }
-            mHandler.postDelayed(
-                adsRunner,
-                (1000)
-            )
         } else {
             binding.loadingProgress.visibility = View.GONE
             binding.btnNext.visibility = View.VISIBLE
